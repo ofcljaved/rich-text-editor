f8a6d8f61bfb679b18ae55035a20f9eac34aa8cf
Javed <javedofficialme@gmail.com>
2023-12-05 22:08:24 +0530

    test : writing content with documnet API

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,15 +1,30 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const TextInput = () => {
   const [content, setContent] = useState<string>("");
+  const contentEditableRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (contentEditableRef.current) {
+      const range = document.createRange();
+      const selection = document.getSelection();
+      range.selectNodeContents(contentEditableRef.current);
+      range.collapse(false);
+      selection?.removeAllRanges();
+      selection?.addRange(range);
+    }
+  }, [content]);
+
   return (
     <div
+      ref={contentEditableRef}
       contentEditable
       onInput={(e) => {
         setContent(e.currentTarget.innerHTML);
       }}
       dangerouslySetInnerHTML={{ __html: content }}
-      className="p-4 w-1/2 h-1/2 bg-gray-100"
+      spellCheck
+      className="p-4 w-1/2 h-1/2 bg-gray-100 whitespace-pre-wrap"
     ></div>
   );
 };
